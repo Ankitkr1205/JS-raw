7c69728fcdb8491207f773e47a0e1349994fab66
Ankit Chaudhary <chaudharyankit573nets@gmail.com>
2024-01-04 18:56:08 +0530

    functions

---
+++ Chapter-1/functionsBasic.js
@@ -0,0 +1,52 @@
+// console.log("We are learning functions");
+
+//What is functions
+
+/*
+1)A javascript function is a block of code 
+designed to perform a particular task
+
+2)A javascript function is executed when "something" invokes it(calls it)
+
+3)A javascript function is defined with "function" keyword,
+follwed by a name, followed by a parentheses
+
+4)Function name is same as variable name
+
+5)The  paretheses may include "Parameter" names separated by commas
+
+6)The code to be executed by the functions , 
+is placed inside curly brackets{}
+
+7)Function "parameters" are listed inside the paretheses()
+in function defination
+
+8)Function "arguments" are the values recieved by the function 
+when it is invoked or called
+
+9)Inside the function, the argument(the parameter) behaves as 
+local varible
+*/
+
+const num1 = 75;
+const num2 = 71;
+console.log("I am global", "num1", num1, "num2", num2);
+
+function addAnyNumber(num1, num2) {
+  console.log("I am local", "num1", num1, "num2", num2);
+  return num1 * num2;
+}
+
+const result = addAnyNumber(2, 3); //function invoke or calling
+console.log("Ans of multiply is :", result);
+document.write("Ans of multiply is :", result);
+
+function studentDetail() {
+  const name1 = "Ankit";
+  const age = 26;
+  const developer = true;
+  const myprofile = "My name is " + name1 + age + developer;
+  return myprofile;
+}
+
+console.log("----------", studentDetail());
